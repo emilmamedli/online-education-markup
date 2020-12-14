e908fa7e4a6c1637f76fdc1d3145c12beb8c43e4
Emil Mamedli <mamedli.emil@gmail.com>
2020-12-15 01:22:50 +0400

    completed login/signup pages and added new pages for teacher cabinet

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,39 @@ $(document).ready(function() {
         $(this).parent('.with-icon').find('input').prop('type', $(this).data('change-to'));
     })
 
+
+
+    $('.more-button').on('click', function(e) {
+        e.stopPropagation();
+        let menu = $(this).parent().find('.sub-menu');
+        $('.table-custom tr.selected').removeClass('selected');
+
+        if ($(menu).is(':visible')) {
+
+            $('.sub-menu').hide();
+
+        } else {
+
+            $('.sub-menu').hide();
+            $(menu).show();
+            $(this).closest('tr').addClass('selected');
+
+        }
+    })
+
+    $(document).click(function() {
+
+        $('.sub-menu').hide();
+
+    })
+
+    $('.tab-button').on('click', function(e) {
+
+        let target = $(this).data('target');
+        $('.tab-button').removeClass('active');
+        $(this).addClass('active');
+        $('.profile-tab-item').hide();
+        $(target).show();
+    })
+
 })
